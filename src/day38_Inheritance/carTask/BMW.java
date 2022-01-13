3a492d177a69736334dc5333b68287a23d65748a
Mohammed <95448569+MohammedAlshalchi@users.noreply.github.com>
2022-01-13 11:04:12 -0500

    new class

---
+++ src/day38_Inheritance/carTask/BMW.java
@@ -0,0 +1,13 @@
+package day38_Inheritance.carTask;
+
+public class BMW extends Car{
+
+    public BMW( String modle, int year, double price, String color, int miles) {
+        super("BMW", modle, year, price, color, miles);
+    }
+
+
+
+
+
+}
---
+++ src/day38_Inheritance/carTask/Tesla.java
@@ -0,0 +1,14 @@
+package day38_Inheritance.carTask;
+
+public class Tesla extends Car{
+    public Tesla( String modle, int year, double price, String color, int miles) {
+        super("Tesla", modle, year, price, color, miles);
+    }
+    public void autoPilot (){
+        System.out.println(brand+" "+model+" is autopilot mode");
+    }
+
+
+
+
+}
