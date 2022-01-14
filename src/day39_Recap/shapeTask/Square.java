5ca8dfde23d6ac939b37d6814c6352bcbb49282e
Mohammed <95448569+MohammedAlshalchi@users.noreply.github.com>
2022-01-14 12:03:34 -0500

    Recap 2

--- src/day39_Recap/shapeTask/Square.java
+++ src/day39_Recap/shapeTask/Square.java
@@ -35,4 +35,25 @@ public class Square extends Shape{
         setSide(side);// more safe , and make sure use the right data EX: we can not use -100 according to the req.
         // of setSide method
     }
+
+    @Override
+    public double area() {
+        //return super.area();
+        return side * side;
+    }
+
+    @Override
+    public double perimeter() {
+        return side * 4;
+    }
+
+    @Override
+    public String toString() {
+        //return super.toString();
+        return "Square{" +
+                "side='" + side + '\'' +
+                ", area='" + area() + '\'' +
+                ", perimeter='" + perimeter() + '\'' +
+                '}';
+    }
 }
