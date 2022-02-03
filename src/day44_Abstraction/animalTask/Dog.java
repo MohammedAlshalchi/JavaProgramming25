3725c830dfb769ed6c291451b512d1c0fc467a47
Mohammed <95448569+MohammedAlshalchi@users.noreply.github.com>
2022-02-03 12:22:56 -0500

    commit 5

---
+++ src/day44_Abstraction/animalTask/Dog.java
@@ -0,0 +1,30 @@
+package day44_Abstraction.animalTask;
+
+public final class Dog extends Animal{// we can make it final BUT we can not inheritance  the class
+/*
+ Dog
+                eat(): eats Pizza
+ */
+    public Dog(String name, String breed, char gender, int age, String size, String color) {
+        super(name, breed, gender, age, size, color);
+    }
+
+    public void bark (){// we can make it final
+    System.out.println(getName()+" is barking");
+}
+
+
+
+    @Override
+    public void eat() {
+        System.out.println(getName()+" eats pizza");
+    }
+
+
+
+
+
+
+
+
+}
