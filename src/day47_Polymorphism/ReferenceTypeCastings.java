d89e662a49a8972a07287ae0a6d91821208cad1d
Mohammed <95448569+MohammedAlshalchi@users.noreply.github.com>
2022-02-16 22:56:29 -0500

    commit 3 day 44 to 45

--- src/day47_Polymorphism/ReferenceTypeCastings.java
+++ src/day47_Polymorphism/ReferenceTypeCastings.java
@@ -21,55 +21,69 @@ Shape shape1 = new Square(5);
         System.out.println( ( (Square)shape1).getSide() );
         // WebDriver driver = (WebDriver) new ChromeDriver();
 
-        // Dog  dog2 = new Dog("Max", "Husky", 'M', 3, "Small", "White");;
+
+
+         Dog  dog = new Dog("Max", "Husky", 'M', 3, "Small", "White");;
         //  Dog dog3 = dog2;
 
         Animal animal = new Dog("Max","Husky",'M',3,"Small","White");
-//   Dog dog = (Dog)animal; // downcasting
-        //  dog.bark();
+//   Dog dog = (Dog)animal; // downcasting with variable we can use it later
+
+
+        //animal.bark();
+         dog.bark();
 
         System.out.println(animal.getName());
         // System.out.println( dog.getName() );
 
-        (  (Dog)animal  ).bark();
+
+        (  (Dog)animal  ).bark();// if we do not to use the variable again
+
+
+
 
 
         Shape shape3 = new Square(4);
-       // System.out.println((Square)shape3.getName());
 
+
+
+        System.out.println(((Square) shape3).getSide());
 
 
 
         System.out.println("==========================+++++++++++++++++++++++++++++++");
 
         Animal animal2 = new Cat("Jim","Scotish",'M',3,"Small","White");
-        Cat cat = (Cat) animal2;
+        Cat cat = (Cat) animal2;// if we want to use the variable (cat) again
         cat.eat();
 
         animal2.eat();
 
-        ((Cat) animal2).meow();
+        ((Cat) animal2).meow();// to use one time
 
-       // ((Dog) animal2)  .bark();
+       // ((Dog) animal2)  .bark();===>NO IS A relation between Dog and Cat
 
         System.out.println("==========================+++++++++++++++++++++++++++++++");
+
+
         Employee employee = new Tester("Ali",30,'M',42,"SDET",140000);
 
        ((Tester) employee).bugReport();
-        //  ( (Tester)employee ).unitTest();
+        //  ( (Tester)employee ).unitTest();===> tester has no unitTest method
 
-        //  ( (Developer)employee ).unitTest();
+        //  ( (Developer)employee ).unitTest();====>It will work but It will through exception becase there is NO
+        // IS-A relation between Developer and Tester
 
-//  Driver driver = (Driver) employee;  // line 1
-
-        Person person = (Person) employee ;
-        //    Teacher teacher = (Teacher) employee; // line 3
+            //Driver driver = (Driver) employee;  // line 1
+              Person person = (Person) employee ;//line2
+            //Teacher teacher = (Teacher) employee; // line 3====> tester is not teacher NO relation
 
         System.out.println("==========================+++++++++++++++++++++++++++++++");
 
        Shape s1= new Circle(10) ;
-       s1.perimeter();
-        //  ( (Cube)s1 ).volume();
+            s1.area();
+            s1.perimeter();
+        //  ( (Cube)s1 ).volume();====> we can run it BUT there is  NO IS-A  relation
 
 
 
